f84e3177bf7817dc14f3799a8c85ee97701024ee
HZKD513 <544228362@qq.com>
2022-09-03 21:58:46 +0800

    添加项目文件。

---
+++ test_2022_9_4/2022_9_4.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+#include<string.h>
+
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = { 'a','b','c','d','e','f' };
+//
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//
+//	printf("%d", (n / 12) * 4 + 2);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchaer()) != EOF)
+//	{
+//		if (ch >= 'A' && ch <= 'Z')
+//		{
+//			ch += 32;
+//		}
+//		printf("%c\n", ch);
+//
+//		getchar();
+//	}
+//	return 0;
+//}
+
+
+//int Fun(int n)
+//{
+//    if (n == 5)
+//        return 2;
+//    else
+//        return 2 * Fun(n + 1);
+//}
+//
+//int main()
+//{
+//    int ret = Fun(2);
+//
+//    printf("%d", ret);
+//
+//    return 0;
+//}
+
+
+
+
+
